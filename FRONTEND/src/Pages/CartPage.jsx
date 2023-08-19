0b20fcab8e6ddca136f1a63cb4092c39ea910797
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-19 16:57:34 +0530

    UI: Showing Empty Cart

--- FRONTEND/src/Pages/CartPage.jsx
+++ FRONTEND/src/Pages/CartPage.jsx
@@ -4,16 +4,23 @@ import CartContainer from "../Components/CartPage/CartContainer";
 import CartSummery from "../Components/CartPage/CartSummery";
 import useFetch from "../Hooks/useFetch";
 import { CART } from "../Api/endpoints";
+import { useSelector } from "react-redux";
+import EmptyCart from "../Components/CartPage/UI/EmptyCart";
 
 function CartPage() {
   const fetchedCartItems = useFetch(CART, true);
+  const { total } = useSelector((state) => state.cartSlice);
 
   return (
     <PageWrapper className="px-2">
-      <div className="flex flex-col md:flex-row">
-        <CartContainer fetchedCartItems={fetchedCartItems} />
-        <CartSummery />
-      </div>
+      {total.quantity > 0 ? (
+        <div className="flex flex-col md:flex-row">
+          <CartContainer fetchedCartItems={fetchedCartItems} />
+          <CartSummery />
+        </div>
+      ) : (
+        <EmptyCart />
+      )}
     </PageWrapper>
   );
 }
