def3c658767ff9495911d7c6cca20e0fd1992fc6
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-20 11:18:43 +0530

    FIX: Main Category Image

--- FRONTEND/src/Components/HomePage/UI/MainCategory.jsx
+++ FRONTEND/src/Components/HomePage/UI/MainCategory.jsx
@@ -4,9 +4,9 @@ import { NavLink } from "react-router-dom";
 function MainCategory({ name, image, id }) {
   return (
     <NavLink to={`/pl/${id}/1`}>
-      <div className="  shrink-0 w-28 h-40 gap-3  flex flex-col justify-center items-center">
+      <div className="hover:bg-green-50 transition-all bg-white  shrink-0 w-28 h-40 gap-3  flex flex-col justify-center items-center">
         <img
-          className=" w-full h-[60%] object-cover object-center"
+          className="  w-20  object-cover object-center"
           src={image}
           alt="mainCategory"
         />
