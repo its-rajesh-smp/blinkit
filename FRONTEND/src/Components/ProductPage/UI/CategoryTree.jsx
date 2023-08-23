9bce2a1fcf1aef6b97f932219a4f7bde7e785bed
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-23 09:12:54 +0530

    FEAT: Dynamic Tree Navigation from product page

--- FRONTEND/src/Components/ProductPage/UI/CategoryTree.jsx
+++ FRONTEND/src/Components/ProductPage/UI/CategoryTree.jsx
@@ -1,13 +1,23 @@
 import React, { useContext } from "react";
 import ProductDetailsContext from "../../../Context/ProductDetailsPageContext";
+import { Link } from "react-router-dom";
 
 function CategoryTree() {
   const { mainCategory, subCategory } = useContext(ProductDetailsContext);
+
   return (
     mainCategory && (
-      <p className="text-xs">
-        <span>Home</span>/ <span>{mainCategory.name}</span> /{" "}
-        <span>{subCategory.name}</span>
+      <p className="text-xs flex gap-1 ">
+        <span className=" hover:text-green-800  text-green-500">
+          <Link to="/">Home</Link>
+        </span>
+        /
+        <span className=" hover:text-green-800  text-green-500">
+          <Link to={`/pl/${mainCategory.id}/${subCategory.id}`}>
+            {mainCategory.name}
+          </Link>
+        </span>
+        / <span className=" text-green-800  ">{subCategory.name}</span>
       </p>
     )
   );
