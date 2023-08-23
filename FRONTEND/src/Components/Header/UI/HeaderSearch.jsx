cca099c55e77a4e1817a6e77b51ba3f729593790
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-08-23 09:03:36 +0530

    FEAT: Dynamic Search Bar Placeholder

--- FRONTEND/src/Components/Header/UI/HeaderSearch.jsx
+++ FRONTEND/src/Components/Header/UI/HeaderSearch.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 
 function HeaderSearch() {
+  const dynamicNames = ["egg", "ganja", "paan", "daaru"];
+  const [index, setIndex] = useState(0);
+  const timerRef = useRef();
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setIndex((p) => {
+        if (p === dynamicNames.length - 1) {
+          return 0;
+        } else {
+          return p + 1;
+        }
+      });
+    }, 1000);
+
+    return () => {
+      clearInterval(timerRef.current);
+    };
+  }, []);
+
   return (
-    <div className=" col-start-1 col-span-5 md:col-auto md:col-start-auto w-full shrink rounded-md px-3 bg-gray-100 flex items-center">
+    <div className=" col-start-1 overflow-hidden col-span-5 md:col-auto md:col-start-auto w-full shrink rounded-md px-3 bg-gray-100 flex items-center">
       <AiOutlineSearch className="  text-2xl " />
       <input
-        className=" w-full bg-gray-100 ml-3 h-12"
+        disabled
+        className=" transition-all w-full bg-gray-100 ml-3 h-12"
         type="text"
-        placeholder="Searh 'egg'"
+        placeholder={`Search '${dynamicNames[index]}'`}
       />
     </div>
   );
